f2837caad25452a04827cb9fbad8547357d7f094
cnrtrgt1453 <canerturgut34@gmail.com>
2019-09-27 17:15:05 +0300

    Bu committe Console a yazıları biz girdik, programın içerisinde hazır olarak 3 tane integer kayıtlı değildi, klavyeden girdiğimiz en küçük sayıdan en büyüğünü bulan kodu yazmış olduk.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	int sayi1=50;
-	int sayi2=25;
-	int sayi3=3;
-	int enBuyuk=sayi1;
-	if (enBuyuk<sayi2){
+
+		Scanner tarayici=new Scanner(System.in);
+
+		System.out.println("Birinci sayiyi giriniz: ");
+	    int sayi1=tarayici.nextInt();
+
+		System.out.println("İkinci sayiyi giriniz: ");
+	    int sayi2=tarayici.nextInt();
+
+		System.out.println("Üçüncü sayiyi giriniz: ");
+	    int sayi3=tarayici.nextInt();
+
+
+	    int enBuyuk=sayi1;
+
+	    if (enBuyuk<sayi2){
 	    enBuyuk=sayi2;
+        }
+
+	    if (enBuyuk<sayi3){
+         enBuyuk=sayi3;
+        }
+
+	    if (sayi1==sayi2 && sayi1==sayi3){
+			System.out.println("Tüm sayilari eşit girdiniz");
+		}else {
+			System.out.println("En büyük = " + enBuyuk);
+		}
+		}
     }
 
-	if (enBuyuk<sayi3){
-      enBuyuk=sayi3;
-    }
-
-        System.out.println("En büyük = "+enBuyuk);
-    }
-}
